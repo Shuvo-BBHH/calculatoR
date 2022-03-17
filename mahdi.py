4a2844e8f4d3bbde282e177afb7029ae17ef2f34
Mahdi Hasan Shuvo <98658558+Shuvo-BBHH@users.noreply.github.com>
2022-03-17 15:33:42 +0600

    Create mahdi.py

---
+++ mahdi.py
@@ -0,0 +1,30 @@
+os.system("clear")
+print("""                    oooo                        oooo                .  \033[4;31m          
+                    `888                        `888              .o8                      
+ .ooooo.   .oooo.    888   .ooooo.  oooo  oooo   888   .oooo.   .o888oo  .ooooo.  oooo d8b \033[4;32m
+d88' `"Y8 `P  )88b   888  d88' `"Y8 `888  `888   888  `P  )88b    888   d88' `88b `888""8P \033[4;33m
+888        .oP"888   888  888        888   888   888   .oP"888    888   888   888  888 \033[4;34m
+888   .o8 d8(  888   888  888   .o8  888   888   888  d8(  888    888 . 888   888  888  \033[4;35m
+`Y8bod8P' `Y888""8o o888o `Y8bod8P'  `V88V"V8P' o888o `Y888""8o   "888" `Y8bod8P' d888b   \033[1;37m
+                                                                                           
+                                                                                           
+                     8 DISITE                                                             
+\033[1;34m===============================================================
+CREATED BY\033[1;31mMAHDI HASAN(SHUVO)\033[0;16m
+FB ; https://web.facebook.com/mahdi80808
+\033[1;33mFB Grup ;https://web.facebook.com/groups/610487559129086
+================================================================\033[0;14m""")
+
+
+
+
+a=input("input 1No number:\033[93m")
+b=input("\033[1;202No number:\033[1;34m")
+c=input("input 3NOnumber:\033[1;31m")
+d=input("input 4No number:\033[4;32m")
+e=input("input 5NOnumber:\033[4;33m")
+f=input("input 6NOnumber:\033[1;36m")
+g=input("input 7NO mber:\033[4;32m \033[4;33m")
+h=input("input 8NOnumber:\033[4;36m")
+
+print("Result:\033[0;31m"+str(int(a)+int(b)+int(c)+int(d)+int(e)+int(f)+int(g)+int(h)))
